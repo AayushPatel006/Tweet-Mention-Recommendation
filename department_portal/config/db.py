ea9a4952fbf47692e40a3b7602dcbd6fc70445d6
Chaitya Dobariya <112513370+Chaitya02@users.noreply.github.com>
2024-09-20 19:16:32 +0530

    Add files via upload

--- department_portal/config/db.py
+++ department_portal/config/db.py
@@ -1,1 +1,13 @@
+import os
+from pymongo import MongoClient
+from dotenv import load_dotenv
 
+load_dotenv()
+
+USER = os.environ.get("DB_USER")
+USER_PASSWORD = os.environ.get("DB_USER_KEY")
+
+MONGO_URI = (f"mongodb+srv://{USER}:{USER_PASSWORD}@cluster0.mkddwv8.mongodb.net"
+             "/vrpDB?retryWrites=true&w=majority")
+
+client = MongoClient(MONGO_URI)
